049ca31a5447d104716dc2fcb40e9f5f01daa163
Cameron Mance <ccmance@icloud.com>
2024-04-21 01:13:03 -0700

    Init stuff

--- obj/Debug/net6.0/BattleReport.AssemblyInfo.cs
+++ obj/Debug/net6.0/BattleReport.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattleReport")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87ea03d54c7201cb7a87b56ac785563319a724d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattleReport")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattleReport")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
